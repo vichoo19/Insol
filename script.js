d3b0512f80665a4e638eb47ac4532e2c3df017f4
Vicente <vichopuntocom@gmail.com>
2025-07-30 20:19:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const cornerLogo = document.querySelector('.logo-corner');
     const headerHeight = document.querySelector('header').offsetHeight;
-    const backgroundNav = document.querySelector('.nav');
+    const backgroundNav = document.querySelector('nav');
     
     window.addEventListener('scroll', function() {
         // Calculamos cuánto se ha desplazado la página
@@ -24,3 +24,37 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+
+window.addEventListener('DOMContentLoaded',() => {
+    const logoResto = document.querySelector('.nsol');
+    const logoI = document.querySelector('.i');
+    setTimeout(()=>{
+        logoI.style.transform='translateX(0)'; },600);
+    setTimeout(()=> {
+        logoResto.style.opacity='1';
+        logoResto.style.transform='translateX(0)';},900);
+})
+
+function mostrarServicio(num) {
+    for (let i = 1; i <= 4; i++) {
+        document.getElementById('servicio' + i).style.display = (i === num) ? 'block' : 'none';
+    }
+}
+
+for (let i = 1; i <= 4; i++) {
+    const botonToggle = document.getElementById('boton-toggle' + i);
+    botonToggle.addEventListener('click', () => {
+        for (let j = 1; j <= 4; j++) {
+            const otroBoton = document.getElementById('boton-toggle' + j);
+            if (j !== i) {
+                otroBoton.classList.remove('active');
+            }
+        }
+        botonToggle.classList.toggle('active');
+    });
+}
+
+
+window.onload = function() {
+    mostrarServicio(1);
+};
